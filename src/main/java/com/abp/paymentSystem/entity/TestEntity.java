bee64dde6481f5c658475131e796dd0ce2309c2b
anisa707 <anisa_hajrullaj@yahoo.com>
2019-10-12 14:38:17 +0200

    student and faculty entity,service and repository is created

--- src/main/java/com/abp/paymentSystem/entity/TestEntity.java
+++
@@ -1,6 +0,0 @@
-package com.abp.paymentSystem.entity;
-
-
-public class TestEntity {
-
-}
---
+++ src/main/java/com/abp/paymentSystem/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.abp.paymentSystem.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.abp.paymentSystem.entity.Student;
+
+public interface StudentRepository extends CrudRepository<Student,Long>{
+
+}
