b0482d658e6fee9a214d6e20e69831770a2cb27d
erharizi1 <eharizi120@gmail.com>
2019-10-16 12:34:50 +0200

    login implementation

---
+++ src/main/java/com/abp/paymentSystem/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.abp.paymentSystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class UserController {
+	
+	@RequestMapping(value = {"/login"},method=RequestMethod.GET)
+	public ModelAndView login() {
+		ModelAndView model = new ModelAndView();
+		model.setViewName("user/login2");
+		return model;
+	}
+	
+
+}
